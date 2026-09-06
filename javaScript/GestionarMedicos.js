5d1ec087f991dee1b114e6175566ecbd8df2c49f
Manuel Mora <manolitoking7@gmail.com>
2026-09-06 05:07:33 -0300

    se añade funcion de mostrar todos los medicos en el sistema en modo administrador

--- javaScript/GestionarMedicos.js
+++ javaScript/GestionarMedicos.js
@@ -1,1 +1,24 @@
-console.log(profesionales)
+function gestionarMedicos() {
+    let medicos = document.getElementById("listaMedicos")
+
+    for (let i = 0; i < profesionales.length; i++) {
+        medicos.innerHTML += `
+
+                
+            <tr>
+                <td>${profesionales[i].nombre}</td>
+                <td>${profesionales[i].especialidad}</td>
+                <td>Activo</td>
+                <td>
+                    <button>Editar</button>
+                    <button>Eliminar</button>
+
+                </td>
+            </tr>
+        `
+    }
+}
+gestionarMedicos()
+
+
+
