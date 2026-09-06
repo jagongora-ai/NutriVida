998b060a2500f2fb1130ca39d0eaab8917a880c3
Manuel Mora <manolitoking7@gmail.com>
2026-09-06 20:10:44 -0300

    se agrega funcionalidad de agregar nuevo medico

--- javaScript/GestionarMedicos.js
+++ javaScript/GestionarMedicos.js
@@ -1,10 +1,8 @@
 function gestionarMedicos() {
     let medicos = document.getElementById("listaMedicos")
-
+    medicos.innerHTML =  ""
     for (let i = 0; i < profesionales.length; i++) {
         medicos.innerHTML += `
-
-                
             <tr>
                 <td>${profesionales[i].nombre}</td>
                 <td>${profesionales[i].especialidad}</td>
@@ -20,5 +18,44 @@ function gestionarMedicos() {
 }
 gestionarMedicos()
 
+function agregarMedico() {
+
+    let nombre = document.getElementById("nombreMedico").value
+    let especialidad = document.getElementById("especialidad").value
+    let atencion = document.getElementById("tipoAtencion").value
+    let experiencia = document.getElementById("experienciaLaboral").value
+    let titulos = document.getElementById("estudios").value
+
+    let id = 0
+
+    for(let i = 0; i < profesionales.length; i++){
+
+        if(profesionales[i].id > id){
+            id = profesionales[i].id
+        }
+
+    }
+    let idNuevo = id + 1 
+
+    let medicoNuevoAgregado ={
+
+        id : idNuevo,
+        nombre,
+        especialidad,
+        atencion,
+        experiencia,
+        ubicacion :"Temuco,chile",
+        sobreMi: "Medico nuevo Nutrivida",
+        estudios : [titulos],
+        areas : []
+    }
+    profesionales.push(medicoNuevoAgregado);
+    localStorage.setItem(
+        "profesionales",
+        JSON.stringify(profesionales)
+    );
+    gestionarMedicos();
+
+}
 
 
