80ba507c814cfbc601469faab489c083499cacfb
Manuel Mora <manolitoking7@gmail.com>
2026-09-06 22:04:48 -0300

    Se añade funcionamiento de eliminar y actualizar medicos

--- javaScript/GestionarMedicos.js
+++ javaScript/GestionarMedicos.js
@@ -1,20 +1,22 @@
 function gestionarMedicos() {
     let medicos = document.getElementById("listaMedicos")
-    medicos.innerHTML =  ""
+    medicos.innerHTML = ""
     for (let i = 0; i < profesionales.length; i++) {
         medicos.innerHTML += `
             <tr>
                 <td>${profesionales[i].nombre}</td>
                 <td>${profesionales[i].especialidad}</td>
+                <td>${profesionales[i].experiencia}</td>
                 <td>Activo</td>
                 <td>
-                    <button>Editar</button>
-                    <button>Eliminar</button>
+                    <button onclick = "editar(${profesionales[i].id})">Editar</button>
+                    <button onclick = "eliminarMedico(${profesionales[i].id})">Eliminar</button>
 
                 </td>
             </tr>
         `
     }
+
 }
 gestionarMedicos()
 
@@ -28,26 +30,26 @@ function agregarMedico() {
 
     let id = 0
 
-    for(let i = 0; i < profesionales.length; i++){
+    for (let i = 0; i < profesionales.length; i++) {
 
-        if(profesionales[i].id > id){
+        if (profesionales[i].id > id) {
             id = profesionales[i].id
         }
 
     }
-    let idNuevo = id + 1 
+    let idNuevo = id + 1
 
-    let medicoNuevoAgregado ={
+    let medicoNuevoAgregado = {
 
-        id : idNuevo,
+        id: idNuevo,
         nombre,
         especialidad,
         atencion,
         experiencia,
-        ubicacion :"Temuco,chile",
+        ubicacion: "Temuco,chile",
         sobreMi: "Medico nuevo Nutrivida",
-        estudios : [titulos],
-        areas : []
+        estudios: [titulos],
+        areas: []
     }
     profesionales.push(medicoNuevoAgregado);
     localStorage.setItem(
@@ -57,5 +59,63 @@ function agregarMedico() {
     gestionarMedicos();
 
 }
+function eliminarMedico(eliminarId) {
+    let posicion
+    for (let i = 0; i < profesionales.length; i++) {
 
+        if (eliminarId == profesionales[i].id) {
+            posicion = i
+        }
+    }
+    profesionales.splice(posicion, 1)
+    localStorage.setItem(
+        "profesionales",
+        JSON.stringify(profesionales)
+    );
+    gestionarMedicos();
+}
+
+let idEdicion;
+function editar(id) {
+    idEdicion = id
+    let medicoEncotrado
+    for (let i = 0; i < profesionales.length; i++) {
+        if (id == profesionales[i].id) {
+            medicoEncotrado = profesionales[i]
+        }
+    }
+    document.getElementById("nombreMedico").value = medicoEncotrado.nombre
+    document.getElementById("especialidad").value = medicoEncotrado.especialidad
+    document.getElementById("tipoAtencion").value = medicoEncotrado.atencion
+    document.getElementById("experienciaLaboral").value = medicoEncotrado.experiencia
+    document.getElementById("estudios").value = medicoEncotrado.estudios
+}
+
+
+function guardarCambios() {
+
+    let nombre = document.getElementById("nombreMedico").value
+    let especialidad = document.getElementById("especialidad").value
+    let atencion = document.getElementById("tipoAtencion").value
+    let experiencia = document.getElementById("experienciaLaboral").value
+    let titulos = document.getElementById("estudios").value
+    let posicionn
+    for (let i = 0; i < profesionales.length; i++) {
+
+        if (idEdicion == profesionales[i].id) {
+            posicionn = i
+        }
+    }
+    profesionales[posicionn].nombre  = nombre
+    profesionales[posicionn].especialidad = especialidad
+    profesionales[posicionn].atencion = atencion
+    profesionales[posicionn].experiencia = experiencia
+    profesionales[posicionn].estudios = [titulos]
+
+    localStorage.setItem(
+        "profesionales",
+        JSON.stringify(profesionales)
+    );
+    gestionarMedicos()
+}
 
